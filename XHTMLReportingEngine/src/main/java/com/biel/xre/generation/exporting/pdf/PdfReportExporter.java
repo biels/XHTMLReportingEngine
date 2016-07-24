ba42cda60880e2f036a5a7d3c658561d747c6115
Biel Simon Rojas <bielsimon2@gmail.com>
2016-07-24 21:17:40 +0200

    Creatd PdfReportExporter

---
+++ XHTMLReportingEngine/src/main/java/com/biel/xre/generation/exporting/pdf/PdfReportExporter.java
@@ -0,0 +1,21 @@
+package com.biel.xre.generation.exporting.pdf;
+
+import java.io.File;
+
+import com.biel.xre.generation.Report;
+import com.biel.xre.generation.exporting.ReportExporter;
+
+public class PdfReportExporter extends ReportExporter {
+
+	public PdfReportExporter(Report report) {
+		super(report);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void export(File output) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
