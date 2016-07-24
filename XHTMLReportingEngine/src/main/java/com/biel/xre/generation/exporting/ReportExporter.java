1166fcedadd29316c811ecf7bb1915097d1c03c4
Biel Simon Rojas <bielsimon2@gmail.com>
2016-07-24 19:04:17 +0200

    Created base report exporter

---
+++ XHTMLReportingEngine/src/main/java/com/biel/xre/generation/exporting/ReportExporter.java
@@ -0,0 +1,25 @@
+package com.biel.xre.generation.exporting;
+
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.biel.xre.generation.Report;
+
+public abstract class ReportExporter {
+	final Logger l = LoggerFactory.getLogger(ReportExporter.class);
+	protected Report report;
+	protected String xhtml;
+	
+	public ReportExporter(Report report) {
+		super();
+		if(report == null)l.error("Tried to create a ReportExporter with a null report");
+		this.report = report;
+		l.debug("Populating xhtml string...");
+		this.xhtml = report.getXHTML();
+		l.debug("ReportExporter for report {} is ready", report.getClass().getName());
+	}
+
+	public abstract void export(File output);
+}
