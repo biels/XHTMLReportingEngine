4d387753b59546411ebb7a7c104f30c2f160fc99
Biel Simon Rojas <bielsimon2@gmail.com>
2016-08-02 10:56:08 +0200

    Updated linear layout to use ArrayList instead of array

--- XHTMLReportingEngine/src/main/java/com/biel/xre/generation/xhtml/LinearLayout.java
+++ XHTMLReportingEngine/src/main/java/com/biel/xre/generation/xhtml/LinearLayout.java
@@ -1,27 +1,28 @@
 package com.biel.xre.generation.xhtml;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import com.biel.xre.generation.XHTMLFragment;
 
 public class LinearLayout implements XHTMLFragment {
-	XHTMLFragment[] fragments;
+	ArrayList<? extends XHTMLFragment> fragments;
 	
 	
 	public LinearLayout(XHTMLFragment... fragments) {
 		super();
-		this.fragments = fragments;
+		this.fragments = new ArrayList<XHTMLFragment>(Arrays.asList(fragments));
 	}
 	public LinearLayout(ArrayList<? extends XHTMLFragment> fragments) {
 		super();
-		this.fragments = fragments.toArray(new XHTMLFragment[0]);
+		this.fragments = fragments;
 	}
 
 	@Override
 	public String getXHTML() {
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < fragments.length; i++) 
-			sb.append(fragments[i].getXHTML());
+		for (int i = 0; i < fragments.size(); i++) 
+			sb.append(fragments.get(i).getXHTML());
 		return sb.toString();
 	}
 
