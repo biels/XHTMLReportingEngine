7aea75e02257225dc0069e560891f3965973eecc
Biel Simon Rojas <bielsimon2@gmail.com>
2016-08-23 16:04:47 +0200

    Updated generation api

--- XHTMLReportingEngine/src/main/java/com/biel/xre/generation/TableFragment.java
+++ XHTMLReportingEngine/src/main/java/com/biel/xre/generation/TableFragment.java
@@ -1,29 +1,74 @@
 package com.biel.xre.generation;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import com.biel.xre.generation.xhtml.LinearLayout;
 import com.biel.xre.generation.xhtml.Tag;
 
-public abstract class TableFragment extends XHTMLReportFragment {
-	TableHeader header;
-	TableBody body;
-	
+public class TableFragment extends XHTMLReportFragment {
+	List<TableRow> rows = new ArrayList<>();
 	@Override
 	public String getXHTML() {
-		return new Tag("table", new LinearLayout(header, body)).getXHTML();
+		return new Tag("table", new LinearLayout(rows)).getXHTML();
 	}
-	abstract class TableHeader extends XHTMLReportFragment{
-		
-	}
-	abstract class TableBody extends XHTMLReportFragment{
-		ArrayList<TableRow> rows = new ArrayList<TableRow>();
+	public class TableRow extends XHTMLReportFragment{
+		List<XHTMLFragment> columns = new ArrayList<>();
+
+		public TableRow(List<XHTMLFragment> columns) {
+			super();
+			this.columns = columns;
+		}
+		public TableRow(XHTMLFragment... columns) {
+			super();
+			this.columns = Arrays.asList(columns);
+		}
 		@Override
 		public String getXHTML() {
-			return new LinearLayout(rows).getXHTML();
-		}
-		abstract class TableRow extends XHTMLReportFragment{
-			
+			return new Tag("tr", new LinearLayout(columns)).getXHTML();
 		}
 	}
+	public class TableColumn extends XHTMLReportFragment{
+		XHTMLFragment content;
+		
+		public TableColumn(XHTMLFragment content) {
+			super();
+			this.content = content;
+		}
+
+		@Override
+		public String getXHTML() {
+			// TODO Auto-generated method stub
+			return new Tag("td", content).getXHTML();
+		}
+		
+	}
+	public class TableHeaderColumn extends TableColumn{
+		//TODO Add support for styles, saved here
+		public TableHeaderColumn(XHTMLFragment content) {
+			super(content);
+			// TODO Auto-generated constructor stub
+		}
+		@Override
+		public String getXHTML() {
+			// TODO Auto-generated method stub
+			return new Tag("th", content).getXHTML();
+		}
+	}
+	public boolean addRow(List<XHTMLFragment> columns) {
+		TableRow row = new TableRow(columns);
+		return addRow(row);
+	}
+	public boolean addRow(XHTMLFragment... columns) {
+		TableRow row = new TableRow(columns);
+		return addRow(row);
+	}
+	protected boolean addRow(TableRow arg0) {
+		return rows.add(arg0);
+	}
+	protected List<TableRow> getRows() {
+		return rows;
+	}
+	
 }
--- XHTMLReportingEngine/src/main/java/com/biel/xre/generation/XHTMLReport.java
+++ XHTMLReportingEngine/src/main/java/com/biel/xre/generation/XHTMLReport.java
@@ -1,5 +1,6 @@
 package com.biel.xre.generation;
 
+import com.biel.xre.generation.xhtml.LinearLayout;
 import com.biel.xre.generation.xhtml.Tag;
 
 /**
@@ -10,7 +11,12 @@ public abstract class XHTMLReport {
 	//TODO Add basic XHTML boilerplate code
 	//TODO Handle document level CSS
 	public String getFullXHTML(){
-		return new Tag("html", getXHTML()).getXHTML();
+		Tag title = new Tag("title", getClass().getSimpleName());
+		Tag style = new Tag("style", getCSS());
+		Tag head = new Tag("head", new LinearLayout(title, style));
+		Tag body = new Tag("body", getXHTML());
+		return new Tag("html", new LinearLayout(head, body)).getXHTML();
 	}
 	protected abstract String getXHTML();
+	protected abstract String getCSS();
 }
